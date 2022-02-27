de8cf7fb6c2993c8c15db1cb453e3b81f5f9c6e0
Ujjwal Goel <8354378+ujjwal123123@users.noreply.github.com>
2022-02-27 21:01:34 +0530

    Complete Overview section

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import './index.css';
 
 
 class Square extends React.Component {
+  /**
+   * @param {any} props
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  /**
+   * @param {number} i
+   */
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
